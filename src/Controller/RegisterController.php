bdd8892223dc446f3087833d33cfab63a35bf480
Morgan Laversin <morganlaversin@gmail.com>
2021-09-10 09:02:08 +0200

    password hasher

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,13 +9,17 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
     private $entityManager;
+    private $passwordHasher;
 
-    public function __construct(EntityManagerInterface $entityManager) {
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher) {
         $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
     }
 
     /**
@@ -31,6 +35,11 @@ class RegisterController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
             $user = $form->getData();
+
+            $user->setPassword($this->passwordHasher->hashPassword(
+                $user,
+                'password'
+                ));
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
