edcd7cdf6bd1b7324a729a00def525df84b6525b
Morgan Laversin <morganlaversin@gmail.com>
2021-09-14 11:46:34 +0200

    management of user addresses

---
+++ src/Controller/AccountAddressController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Address;
+use App\Form\AddressType;
+use App\Repository\AddressRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class AccountAddressController extends AbstractController
+{
+    /**
+     * @Route("/compte/adresses", name="account_address")
+     */
+    public function index(): Response
+    {
+        return $this->render('account/address.html.twig');
+    }
+
+    /**
+     * @Route("/compte/ajouter-une-adresse", name="account_address_add")
+     */
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $address = new Address();
+
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $address->setUser($this->getUser());
+            $entityManager->persist($address);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/compte/modifier-une-adresse/{id}", name="account_address_edit")
+     */
+    public function edit(Request $request, EntityManagerInterface $entityManager, $id): Response
+    {
+        $address = $this->getDoctrine()->getRepository(Address::class)->findOneById($id);
+
+        if(!$address || $address->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('account_address');
+        }
+
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/compte/supprimer-une-adresse/{id}", name="account_address_delete")
+     */
+    public function delete(EntityManagerInterface $entityManager, $id): Response
+    {
+        $address = $this->getDoctrine()->getRepository(Address::class)->findOneById($id);
+
+        if($address && $address->getUser() == $this->getUser()) {
+            $entityManager->remove($address);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('account_address');
+    }
+}
+
---
+++ src/Form/AddressType.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'adresse',
+                'attr' => [
+                    'placeholder' => 'ex : mon domicile principal, mon travail...'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'ex : Marie'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Durand'
+                ]
+            ])
+            ->add('company', TextType::class, [
+                'label' => 'Votre société',
+                'attr' => [
+                    'placeholder' => 'Ma société (facultatif)'
+                ]
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'ex : 8 rue des Lilas'
+                ]
+            ])
+            ->add('postal', TextType::class, [
+                'label' => 'Code postal',
+                'attr' => [
+                    'placeholder' => 'ex : 75000'
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'ex : PARIS'
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Pays',
+                'preferred_choices' => ['FR'],
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Votre n° de téléphone',
+                'attr' => [
+                    'placeholder' => 'ex : 013258... ou +33678...'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'btn-block btn-info mt-3 mb-5'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
