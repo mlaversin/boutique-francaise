d71917ce970d3c3838421cbdcdadb9bd9b942af6
Morgan Laversin <morganlaversin@gmail.com>
2021-09-20 11:09:21 +0200

    featured products

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,31 @@
 namespace App\Controller;
 
 use App\Classes\Mail;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $products = $this ->entityManager->getRepository(Product::class)->findByIsBest(1);
+
+        return $this->render('home/index.html.twig', [
+            'products' => $products
+        ]);
     }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,13 +5,20 @@ namespace App\Controller;
 use App\Classes\Search;
 use App\Entity\Product;
 use App\Form\SearchType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     /**
      * @Route("/nos-produits", name="products")
@@ -42,15 +49,17 @@ class ProductController extends AbstractController
      */
     public function show($slug): Response
     {
-        $repository = $this->getDoctrine()->getRepository(Product::class);
-        $product = $repository->findOneBy(['slug' => $slug]);
+        $product = $this ->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        
+        $products = $this ->entityManager->getRepository(Product::class)->findByIsBest(1);
 
         if (!$product) {
             return $this->redirectToRoute('products');
         }
 
         return $this->render('product/show.html.twig', [
-            'product' => $product
+            'product' => $product,
+            'products' => $products
         ]);
     }
 }
